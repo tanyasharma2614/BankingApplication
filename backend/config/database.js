098750a02edb0e8c4bbf3eed14c70929de256b67
Tanya <tanyasharma2614@gmail.com>
2023-10-04 20:58:34 -0400

    Setting up database config file for db connection

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+const mysql=require('mysql');
+
+const dbConfig={
+    host:'localhost',
+    user:'root',
+    password:'password',
+    database:'bank',
+};
+
+const connection = mysql.createConnection(dbConfig);
+connection.connect((err)=>{
+    if(err){
+        console.error('Error connecting to database:',err);
+    }else{
+        console.log('Connected to the database');
+    }
+});
+module.exports=connection;
