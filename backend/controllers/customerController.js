cee5d9d9540f15dcd29a44c3739002816c250726
Anmol Arora <anmol.arora8991@gmail.com>
2023-10-19 15:37:10 -0400

    handling signup and bankstatement requests from frontend

--- backend/controllers/customerController.js
+++ backend/controllers/customerController.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const bcrypt = require('bcrypt');
 const Customer = require('../models/customer');
 
 const customerController = {
@@ -28,48 +29,107 @@ const customerController = {
       }
     });
   },
-  // Add more controller methods for other customer-related APIs here
   sign_up: function(req, res){
     let body = '';
     req.on('data', chunk => {
       body += chunk;
     });
-    req.on('end', () => {
-      console.log(body)
-        console.log(JSON.parse(body));
-        res.end('ok');
-    });
-    // Expected data from frontend
-    // { 'f-name': "Vanna Poole",
-    // 'l-name': "Zenaida Levy",
-    // 'mail': "carywoh@mailinator.com",
-    // 'phone': "+1 (821) 518-6295",
-    // 'user-password': "Pa$$w0rd!",
-    // 'user-password-confirm': "Pa$$w0rd!"}
+    req.on('end', async () => {
+        console.log(`Data received on backend: ${body}`)
+        // Expected data from frontend
+        // {
+        //   'c-id': 'Aperiam libero volup',
+        //   'u-name': 'Kaitlin Mcintosh',
+        //   'user-password': 'Pa$$w0rd!',
+        //   'user-password-confirm': 'Pa$$w0rd!'
+        // }
+        const requestData = JSON.parse(body);
+        const cust_id = parseInt(requestData['c-id']);
+        const username = requestData['u-name'];
+        const password = await bcrypt.hash(requestData['user-password'], 10); // 10 is the number of salt rounds
 
-    const cust_id = req.headers.customer_id;
-    const username = req.headers.username;
-    const password = req.headers.password;
-
-    if (!cust_id || !username || !password) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'Missing customer id, username or password in headers' }));
-      return;
-    }
+        console.log(`${cust_id}, ${username}, ${password}`)
     
-    Customer.signup(cust_id, username, password, (error, results) => {
-      if(error){
-        console.error(error);
-        res.writeHead(500, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'Internal Server Error' }));
-      }
-      else{
-        console.log("1 record inserted via Sign-up");
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ success: true }));
-      }
+        if (!cust_id || !username || !password) {
+          res.writeHead(400, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Missing customer id, username or password in headers' }));
+        }else{
+
+          Customer.signup(cust_id, username, password, (error, results) => {
+            if(error){
+              console.error(error);
+              res.writeHead(500, { 'Content-Type': 'application/json' });
+              res.end(JSON.stringify({ error: 'Internal Server Error' }));
+            }
+            else{
+              console.log("1 record inserted via Sign-up");
+              res.writeHead(200, { 'Content-Type': 'application/json' });
+              const responseData = {
+                success: true,
+                message: 'Data Received Successfully',
+                data: {},
+              };
+              res.end(JSON.stringify(responseData));
+            }
+          });
+          
+        }
+        
+        
     });
     
+
+   
+  },
+  bankStatement: function(req, res){
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
+    });
+    req.on('end', async () => {
+        console.log(`Data received on backend: ${body}`)
+        // Expected data from frontend
+        // {
+        //   'c-id': 'Aperiam libero volup',
+        //   'u-name': 'Kaitlin Mcintosh',
+        //   'user-password': 'Pa$$w0rd!',
+        //   'user-password-confirm': 'Pa$$w0rd!'
+        // }
+        const requestData = JSON.parse(body);
+        const cust_id = parseInt(requestData['c-id']);
+
+        // console.log(`${cust_id}`)
+    
+        if (!cust_id) {
+          res.writeHead(400, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Missing customer id, username or password in headers' }));
+        }else{
+
+          Customer.bankStatement(cust_id, (error, results) => {
+            if(error){
+              console.error(error);
+              res.writeHead(500, { 'Content-Type': 'application/json' });
+              res.end(JSON.stringify({ error: 'Internal Server Error' }));
+            }
+            else{
+              console.log(`Result of get query: ${JSON.stringify(results)}`);
+              res.writeHead(200, { 'Content-Type': 'application/json' });
+              const responseData = {
+                success: true,
+                message: 'Data Received Successfully',
+                data: results,
+              };
+              res.end(JSON.stringify(responseData));
+            }
+          });
+          
+        }
+        
+        
+    });
+    
+
+   
   },
   //A function that uses nominatim geolocation API
   locate_branch: function(req, res){
