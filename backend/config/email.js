791ccb3c574124b85e587b301fd958ed312eb37d
Tanya <tanyasharma2614@gmail.com>
2023-10-19 19:05:27 -0400

    Added email notification service module

---
+++ backend/config/email.js
@@ -0,0 +1,36 @@
+const nodemailer=require('nodemailer');
+
+const sendEmail=async(to,subject,text,html)=>{
+    const transporter=nodemailer.createTransport({
+        host:'smtp.elasticemail.com',
+        port:2525,
+        secure:false,
+        auth:{
+            user:'BankingApp.SE567@proton.me',
+            pass:'BBF7ABC8D091994A5C0CADADBCC58C6CF204'
+        }
+    });
+
+    const mailOptions={
+        from:'BankingApp.SE567@proton.me',
+        to,
+        subject,
+        text,
+        html
+    };
+
+    try{
+        const info=await transporter.sendMail(mailOptions);
+        console.log('Email sent:',info.response);
+    }catch(error){
+        console.error('Email sending failed:',error);
+    }
+};
+module.exports=sendEmail;
+
+
+sendEmail(
+    'tanyasharma2614@gmail.com',
+    'Test',
+    'This is a test email'
+);
