c501e77143054d0a143de4df83cb25f95a90a9ed
Rishit <rishitnagpal1@gmail.com>
2023-10-19 20:29:56 -0400

    made changes in customerController.js, api.js,customer.js and added accountActivity use case

--- backend/models/customer.js
+++ backend/models/customer.js
@@ -1,25 +1,39 @@
-const db=require('../config/database');
+const db = require("../config/database");
 
-const Customer={
+const Customer = {
+  validateLogin: function (username, password, callback) {
+    const sql = "SELECT * FROM Credentials WHERE username=? AND password=?";
+    db.query(sql, [username, password], callback);
+  },
 
-    validateLogin: function(username, password, callback){
-        const sql='SELECT * FROM Credentials WHERE username=? AND password=?';
-        db.query(sql,[username,password],callback);
-    },
-
-    signup: function(customer_id, username, password, callback){
-        //Note: Need to have the Customer with the current customer_id in the Customer table before this
-        const sql="INSERT INTO Credentials (Customer_Id, User_Type, Username, Password) VALUES (" + customer_id + ", 'Customer', '" + username + "', '" + password + "')";
-        db.query(sql, callback); 
-    },
-    bankStatement: function(customer_id, callback){
-        //Note: Need to have the Customer with the current customer_id in the Customer table before this
-        const sql=` SELECT * FROM Transactions
+  signup: function (customer_id, username, password, callback) {
+    //Note: Need to have the Customer with the current customer_id in the Customer table before this
+    const sql =
+      "INSERT INTO Credentials (Customer_Id, User_Type, Username, Password) VALUES (" +
+      customer_id +
+      ", 'Customer', '" +
+      username +
+      "', '" +
+      password +
+      "')";
+    db.query(sql, callback);
+  },
+  accountActivity: function (customer_id, callback) {
+    //Note: Need to have the Customer with the current customer_id in the Customer table before this
+    const sql = ` SELECT * FROM Transactions
+                    WHERE Customer_Id = ${customer_id}
+                    ORDER BY Timestamp_Start DESC
+                    LIMIT 10;`;
+    db.query(sql, callback);
+  },
+  bankStatement: function (customer_id, callback) {
+    //Note: Need to have the Customer with the current customer_id in the Customer table before this
+    const sql = ` SELECT * FROM Transactions
                     WHERE Customer_Id = ${customer_id}
                     AND Timestamp_Start >= DATE_SUB(NOW(), INTERVAL 3 MONTH)
                     ORDER BY Timestamp_Start DESC;`;
-        db.query(sql, callback); 
-    },
+    db.query(sql, callback);
+  },
 };
 
-module.exports=Customer;
+module.exports = Customer;
